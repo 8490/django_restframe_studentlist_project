d8582f2c9c922985d23a8677017afb63c3a261f8
Mert <mert.san3407@gmail.com>
2021-01-12 23:22:52 +0300

    finished project

---
+++ src/fscohort_api/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import home_api, Student
+
+urlpatterns = [
+    path("home-api/", home_api),
+
+    # path("list-api/", student_list_api),
+    # path("create-api/", student_create_api),
+
+    # path("list-create-api/", student_list_create_api),
+    # path("<int:id>/", student_get_update_delete),
+
+    # path("list-create-api/", StudentList.as_view()),
+    # path("<int:id>/", StudentGetUpdateDelete.as_view(), name="detail"),
+
+    # path("list-create-api/", StudentList.as_view()),
+    # path("<int:id>/", StudentGetUpdateDelete.as_view(), name="detail"),
+
+    path("<int:id>/", Student.as_view(), name="detail"),
+
+
+]
